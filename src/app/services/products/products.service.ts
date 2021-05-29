b224ca18c4bbc773b7ec2b26ef83264e0d2044c5
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-29 15:23:32 +0100

    Todo post product

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
+import { Product } from 'src/app/models/products.model';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  routeProduct= 'http://localhost:3002/products'
+  apiURL= 'http://localhost:3002/products'
+
   constructor() { }
 
   getAllProducts(){
-    return axios.get(this.routeProduct).then(products => products.data.results);
+    return axios.get(this.apiURL).then(products => products.data.results);
   }
-  getProductsUser(){
-    return axios.get(this.routeProduct).then(products => products.data.results);
-  }
-  putProductsUsers(){
-    return axios.put(this.routeProduct+"").then();
+  
+  create(_product : Product){
+    return axios.post(this.apiURL+"/").then(res => {
+      console.log(res.data)
+    }).catch(e => console.error(e));
   }
 }
