cfe52e36403e53786ce6e62ca4e030bec7e572bf
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-06-01 18:28:32 +0100

    Mostrar los datos de los usuarios mediante localstorage

--- src/app/components/banner-user/banner-user.component.ts
+++ src/app/components/banner-user/banner-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-banner-user',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerUserComponent implements OnInit {
 
-  constructor() { }
 
+  user : User = {id: 0, name: '', last_name: '', email: '', password: '', photo_user:'', post_code: 0, ranking: 0, role: '' } 
+  constructor() { 
+      let userLogin = JSON.parse(localStorage.getItem('user') || '{}');
+      let token = localStorage.getItem('token');
+      this.user = userLogin;
+    }
   ngOnInit(): void {
   }
 
--- src/app/views/users/user-products/user-products.component.ts
+++ src/app/views/users/user-products/user-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-products',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProductsComponent implements OnInit {
 
-  constructor() { }
+
+  user : User = {id: 0, name: '', last_name: '', email: '', password: '', photo_user:'', post_code: 0, ranking: 0, role: '' } 
+  constructor() { 
+      let userLogin = JSON.parse(localStorage.getItem('user') || '{}');
+      let token = localStorage.getItem('token');
+      this.user = userLogin;
+    }
 
   ngOnInit(): void {
   }
-
+  
 }
