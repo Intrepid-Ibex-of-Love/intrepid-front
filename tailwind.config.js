6473d29e79d9d2a73b35f8c0a419b55e44a1005b
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-06-16 12:02:18 +0100

    Cambios en el config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        white: "#eff0f3",
+        black: "#0d0d0d",
+        black2: "#2a2a2a",
+        black3: "#0d0d0d",
+        orange: "#ff8e3c",
+        pink: "#d9376e"
+      },
+    },
   },
   variants: {
     extend: {
