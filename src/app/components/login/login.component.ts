881e530628b44dac4605e7eac1335b8d5fa21ac6
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-06-16 12:29:12 +0100

    Cambios en el diseño del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
